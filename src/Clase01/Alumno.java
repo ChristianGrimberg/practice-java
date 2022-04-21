5095934ca6c1aafa17af00fcd04350df55b1a975
Christian Grimberg <christian.grimberg@outlook.com>
2022-04-21 00:29:15 +0000

    :monkey_face: Learn how to create and import a class.

---
+++ src/Clase01/Alumno.java
@@ -0,0 +1,9 @@
+package Clase01;
+
+public class Alumno {
+    int numeroDocumento;
+
+    public String saludoInicial() {
+        return "Hola mundo!";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Clase01.Alumno;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Alumno prueba = new Alumno();
+
+        System.out.println(prueba.saludoInicial());
+    }
+}
